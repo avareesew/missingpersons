35416c52ac2e3d6556d9b95c9874dbbef21c9963
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2022-12-07 13:38:18 -0700

    add models

--- missing_persons/missingpersonsapp/models.py
+++ missing_persons/missingpersonsapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Missing_Person(models.Model):
+    date_missing = models.CharField(max_length=40)
+    lastName = models.CharField(max_length=40)
+    firstName = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    state = models.CharField(max_length=2)
+    gender = models.CharField(max_length=1)
+    race = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.lastName + self.firstName
+
+    class Meta :
+        db_table = "Missing Persons"
--- missing_persons/missingpersonsapp/urls.py
+++ missing_persons/missingpersonsapp/urls.py
@@ -4,7 +4,7 @@ from .views import indexPageView, statisticsPageView, bePresentPageView, resourc
 urlpatterns = [
     path('', indexPageView, name= "index"),
     path('statistics/', statisticsPageView, name='statistics'),
-    path('/bepresent', bePresentPageView, name='bepresent'),
-    path('/resources', resourcesPageView, name='resources'),
+    path('bepresent/', bePresentPageView, name='bepresent'),
+    path('resources/', resourcesPageView, name='resources'),
     
 ]
